6aae307e0992c3e68df184cc6ccafb53063fb20a
Jeremy Lam <whoisjeremylam@gmail.com>
2026-08-13 03:46:06 +0000

    chore: report TERM_PROGRAM as remoteterm across the fork
    Brand the fork's shells (local and remote SSH via swap token) with
    TERM_PROGRAM=remoteterm instead of waveterm. Updates the swap-token
    env, local shell env vars, and the test-conn diagnostic token.

--- pkg/util/shellutil/shellutil.go
+++ pkg/util/shellutil/shellutil.go
@@ -221,7 +221,7 @@ func WaveshellLocalEnvVars(termType string) map[string]string {
 		rtn["TERM"] = termType
 	}
 	// these are not necessary since they should be set with the swap token, but no harm in setting them here
-	rtn["TERM_PROGRAM"] = "waveterm"
+	rtn["TERM_PROGRAM"] = "remoteterm"
 	if os.Getenv("COLORTERM") == "" {
 		rtn["COLORTERM"] = "truecolor"
 	}
